cf58909dfe47cf6c89aae96bfb5a5ff6cd5e2886
andriansanto <andriansanto.mb1@gmail.com>
2022-05-03 16:40:06 +0700

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCC8R2gju17eYDpkYsqs91zu3WtLX7KfL8",
+  authDomain: "skripsi-01-97646.firebaseapp.com",
+  databaseURL: "https://skripsi-01-97646-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "skripsi-01-97646",
+  storageBucket: "skripsi-01-97646.appspot.com",
+  messagingSenderId: "150037912656",
+  appId: "1:150037912656:web:f3b6277c5193cf7b6ec555",
+  measurementId: "G-YYWMGCHZHY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const secureRandom = require('secure-random');
+var crypto = require('crypto');
+
+var secureVal = 0;
+
+const app = express();
+
+app.get('/getOTP', async (req, res) => {
+    var Number = getRandomNumber();
+    res.send({"RandomNumber":Number});
+   });
+
+
+function getRandomNumber(){
+    const n = crypto.randomInt(99999, 1000000);
+    var verificationCode = n.toString().padStart(6, "0");
+    return verificationCode;
+}
+
+app.listen(3000);
