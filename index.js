0a4e17c98f8d34512aa1bcf41d0a0ae6066edc2d
andriansanto <andriansanto.mb1@gmail.com>
2022-05-03 20:09:00 +0700

    array

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const rate_limit = require('express-rate-limit');
 const app = express();
 const limiter = rate_limit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
