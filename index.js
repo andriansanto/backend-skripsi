3a26d4f566e19308e02dc3946b16f84f0e9150d6
andriansanto <andriansanto.mb1@gmail.com>
2022-05-03 20:48:53 +0700

    rate ip

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const otp = require('./api/otp');
 const rate_limit = require('express-rate-limit');
+const req_ip = require('request-ip');
 
 const app = express();
 const limiter = rate_limit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 10, // Limit to 10 requests (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    keyGenerator: (req,res) =>{
+        return req.clientIp;
+    }
 });
 const PORT = process.env.PORT || 5050;
+app.use(req_ip.mw());
 app.use(limiter);
 app.use("/api/otp", otp);
 
