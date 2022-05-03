7402e1daeed39c74239557113b7d0b14b0efef9f
andriansanto <andriansanto.mb1@gmail.com>
2022-05-03 20:22:08 +0700

    err msg

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const limiter = rate_limit({
     max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-    message: [{status: 500, RandomNumber: "Your OTP request has exceed the limit, please try again in 15 minute"}],
+    message: "Your OTP request has exceed the limit, please try again in 15 minute",
 });
 const PORT = process.env.PORT || 5050;
 app.use(limiter);
