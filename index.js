4686d6bc45d699033a43d1217b440a8453793731
andriansanto <andriansanto.mb1@gmail.com>
2022-05-03 20:07:21 +0700

    add limiter

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const otp = require('./api/otp');
+const rate_limit = require('express-rate-limit');
 
 const app = express();
-
+const limiter = rate_limit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 const PORT = process.env.PORT || 5050;
-
+app.use(limiter);
 app.use("/api/otp", otp);
 
 app.listen(PORT, ()=> console.log(`server is running in port ${PORT}`));
